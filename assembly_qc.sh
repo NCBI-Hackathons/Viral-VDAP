00c86133475ea465512b2c1e5c8202471e9eff70
Ubuntu <ubuntu@ip-172-16-240-128.ec2.internal>
2019-05-10 17:47:49 +0000

    parsnp code

--- assembly_qc.sh
+++ assembly_qc.sh
@@ -15,3 +15,15 @@ if [ $1 == scaffold ] ; then
 	python ~/Code/utils/qc/asm_assess.py -i ~/data/medusa/$i/$i.contigs.fastaScaffold.fasta
     done
 fi
+
+if [ $1 == trim_mum ] ; then
+    ##trim off ends of contigs using mummer info
+    for i in jsc bc bcbl ;
+    do
+	nucmer -p ~/data/mummer/$i ~/data/ref/gk18.fasta ~/data/medusa/$i/$i.contigs.fastaScaffold.fasta 
+	dnadiff -p ~/data/mummer/$i ~/data/ref/gk18.fasta ~/data/medusa/$i/$i.contigs.fastaScaffold.fasta
+    done
+fi
+
+
+    
